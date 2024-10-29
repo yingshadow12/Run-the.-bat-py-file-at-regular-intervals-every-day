01b597a0151eec8273e72d6b604966f3147a9f4a
yingshadow12 <ying_shadow1220@outlook.com>
2024-10-29 20:43:55 +0800

    Update 每天自动运行.bat脚本.py

--- 每天自动运行.bat脚本.py
+++ 每天自动运行.bat脚本.py
@@ -3,7 +3,7 @@ import time
 import subprocess
 
 def run_bat_file():
-    subprocess.run([r"C:\myszdqd\MihoyoBBSTools-master\&python main_multi.py autorun.bat"], shell=True)
+    subprocess.run([r"C:\your bat.bat"], shell=True)
 
 # 每天20点执行一次 修改20:00修改自动时间
 schedule.every().day.at("20:00").do(run_bat_file)
